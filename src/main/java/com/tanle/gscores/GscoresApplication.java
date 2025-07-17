fcb25543073df7ea90e50d44387357ffdb01a1f3
tanle <fcletan12@gmail.com>
2025-07-17 10:23:22 +0700

    init

---
+++ src/main/java/com/tanle/gscores/GscoresApplication.java
@@ -0,0 +1,13 @@
+package com.tanle.gscores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GscoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanle/gscores/GscoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanle.gscores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GscoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
